0f7307d5afaa3b6465ec5c0572aa9287430a744f
mspremo <167792260+mspremo@users.noreply.github.com>
2024-04-22 16:52:50 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+# defaults 
+HOSTNAME="localhost"
+USERNAME="admin"
+PASSWORD="password123"
+EMAIL="test@example.com"
+STORAGEACCOUNT=""
+CONTAINER=""
+
+for i in "$@"
+do
+	case $i in
+		--hostname=*)
+		HOSTNAME="${i#*=}" 
+		;;
+		--username=*)
+		USERNAME="${i#*=}"
+		;;
+		--password=*)
+		PASSWORD="${i#*=}"
+		;;
+		--email=*)
+		EMAIL="${i#*=}"
+		;;
+		--storageaccount=*)
+		STORAGEACCOUNT="${i#*=}"
+		;;	
+		--container=*)
+		CONTAINER="${i#*=}"
+		;;			
+		*)
+		;;
+	esac
+done
+
+
+#Install Dependencies
+
+apt-get update
+apt-get upgrade -y
+apt-get install -y  php8.1 php8.1-cli php8.1-common php8.1-imap php8.1-redis php8.1-snmp php8.1-xml php8.1-zip php8.1-mbstring php8.1-curl php8.1-gd php8.1-mysql apache2 mariadb-server certbot nfs-common python3-certbot-apache unzip
+
+#Create the database and user
+DBPASSWORD=$(openssl rand -base64 14)
+mysql -e "CREATE DATABASE nextcloud;GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'localhost' IDENTIFIED BY '$DBPASSWORD';FLUSH PRIVILEGES;"
+
+#Mount the file storage
+mkdir -p /mnt/files
+echo "$STORAGEACCOUNT.privatelink.blob.core.windows.net:/$STORAGEACCOUNT/$CONTAINER  /mnt/files    nfs defaults,sec=sys,vers=3,nolock,proto=tcp,nofail    0 0" >> /etc/fstab 
+mount /mnt/files
+
+
+#Download Nextcloud
+cd /var/www/html
+wget https://download.nextcloud.com/server/releases/nextcloud-28.0.4.zip
+unzip nextcloud-28.0.4.zip
+chown -R root:root nextcloud
+cd nextcloud
+
+#Install Nextcloud
+php occ  maintenance:install --database "mysql" --database-name "nextcloud"  --database-user "nextcloud" --database-pass "$DBPASSWORD" --admin-user "$USERNAME" --admin-pass "$PASSWORD" --data-dir /mnt/files
+sed -i "s/0 => 'localhost',/0 => '$HOSTNAME',/g" ./config/config.php
+sed -i "s/  'overwrite.cli.url' => 'https:\/\/localhost',/  'overwrite.cli.url' => 'http:\/\/$HOSTNAME',/g" ./config/config.php
+
+cd ..
+chown -R www-data:www-data nextcloud
+chown -R www-data:www-data /mnt/files
+
+#Configure Apache
+tee -a /etc/apache2/sites-available/nextcloud.conf << EOF
+<VirtualHost *:80>
+ServerName $HOSTNAME
+DocumentRoot /var/www/html/nextcloud
+
+<Directory /var/www/html/nextcloud/>
+ Require all granted
+ Options FollowSymlinks MultiViews
+ AllowOverride All
+ <IfModule mod_dav.c>
+ Dav off
+ </IfModule>
+</Directory>
+
+ErrorLog /var/log/apache2/$HOSTNAME.error_log
+CustomLog /var/log/apache2/$HOSTNAME.access_log common
+</VirtualHost>
+EOF
+
+a2ensite nextcloud.conf
+a2enmod rewrite
+
+#Obtain a Certificate from Let's Encrypt
+certbot run -d $HOSTNAME --agree-tos --apache -m $EMAIL -n
+systemctl restart apache2
